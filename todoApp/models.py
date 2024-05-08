b047d17631cc742a4bcc87c00eeacc3002835d6a
Million D <milliondejene@protonmail.com>
2024-05-08 12:01:25 +0300

    fixed serializer

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)  # Use timezone.now() as the default value
 
     def __str__(self):
         return self.title
--- todoApp/serializers.py
+++ todoApp/serializers.py
@@ -1,8 +1,9 @@
-# serializers.py
+# todo/serializers.py
+
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'description']
